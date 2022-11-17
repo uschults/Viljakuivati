289ffe076271fecb309a7f5c20d417d47a6f6b38
uschults <u.schults@gmail.com>
2022-11-17 21:04:00 +0200

    Create buttontest.py

---
+++ buttontest.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+pin = 7
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN)
+
+while(True):
+    if(GPIO.input(pin)):
+        print("HIGH")
+    else:
+        print("LOW")S
