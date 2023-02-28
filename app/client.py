1a5b725d48eef2f97e50ce5f99a3b6a4372f174a
uschults <90207035+uschults@users.noreply.github.com>
2023-02-28 18:12:02 +0200

    Update client.py

--- app/client.py
+++ app/client.py
@@ -9,24 +9,32 @@ from paho.mqtt import client as mqtt_client
 
 device_folders = []
 
+# to be moved to config file
+broker = '80.250.119.25'
+port = 1883
+topic = "kuivati/temp1"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'urmosc'
+password = 'admin'
+
 
 def temperature_sensor_init():
     os.system('modprobe w1-gpio')
     os.system('modprobe w1-therm')
 
     base_dir = '/sys/bus/w1/devices/'
-    device_folder = glob.glob(base_dir + '28*')
-    print(device_folder)
+    device_folders = glob.glob(base_dir + '28*')
+    print(device_folders)
     #device_file = device_folder + '/w1_slave'
     
-    sub_folders = [name for name in os.listdir('/sys/bus/w1/devices/2*') if os.path.isdir(os.path.join('/sys/bus/w1/devices/', name))]
+    ###if glob doesnt find all folders, use pref scandir or listdir. doesn't find folders with 28* yet..
+    #sub_folders = [name for name in os.listdir('/sys/bus/w1/devices/2*') if os.path.isdir(os.path.join('/sys/bus/w1/devices/', name))]
     #list_subfolders_with_paths = [f.path for f in os.scandir(path) if f.is_dir()]
 
-    print(sub_folders)
+    #print(sub_folders)
 
-    
-
-    return sub_folders
+    return device_folders
 
     
 def connect_mqtt():
@@ -44,17 +52,27 @@ def connect_mqtt():
 
 def mqtt_init():
     client = connect_mqtt()
-    #cl
+    client.loop_start()
+    return client
+
+def publish(client, msg):
+    while True:
+        time.sleep(1)
+        msg = read_temp.read_temperature(device_folders[0]+ '/w1_slave')
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
 
 def main():
-
-    read_temp.read_temperature()
     return 0
 
 
-
 if __name__ == "__main__":
-   # mqtt_init()
+    client = mqtt_init()
     device_folders = temperature_sensor_init()
-
-   # main()
+    publish(client)
+    #main()
