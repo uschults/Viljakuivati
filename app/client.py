788d84a28499bddf3be3d447d4c42a40285d7e08
uschults <90207035+uschults@users.noreply.github.com>
2023-02-28 17:41:02 +0200

    Update client.py

--- app/client.py
+++ app/client.py
@@ -18,7 +18,7 @@ def temperature_sensor_init():
     device_folder = glob.glob(base_dir + '28*')[0]
     device_file = device_folder + '/w1_slave'
     
-    sub_folders = [name for name in os.listdir('/sys/bus/w1/devices/28*') if os.path.isdir(os.path.join('/sys/bus/w1/devices/28*', name))]
+    sub_folders = [name for name in os.listdir('/sys/bus/w1/devices/28*') if os.path.isdir(os.path.join('/sys/bus/w1/devices/28-*', name))]
     print(sub_folders)
 
     
