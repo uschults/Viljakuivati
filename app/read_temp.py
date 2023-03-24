3faf2fb72ab52ed3c884dd2e509caf13d1db6cf7
uschults <u.schults@gmail.com>
2023-03-24 17:35:45 +0200

    Update read_temp.py

--- app/read_temp.py
+++ app/read_temp.py
@@ -8,14 +8,18 @@ def read_temp_raw(device_file):
 
 def read_temperature(device_file):
     lines = read_temp_raw(device_file)
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
+    try:
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c
+    except:
+        print("no tempsensors found!")
+        continue
 
 if __name__ == "__main__":
     print("temp reading")
