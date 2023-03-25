c038e969a83cd5194f39e43326c527688f26cb64
uschults <u.schults@gmail.com>
2023-03-25 21:59:17 +0200

    Create subscribetest.py

---
+++ subscribetest.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+
+broker = '80.250.119.25'
+port = 1883
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'urmosc'
+password = 'admin'
+
+def on_connect(client, userdata, flags, rc):
+     if rc == 0:
+        print("Connected to MQTT Broker!")
+        client.subscribe("mootor")
+        client.subscribe("mootor/mootor1")
+
+
+def on_message(client, userdata, msg):
+    print("Message received-> "+ msg.topic + " " + str(msg.payload))  # Print a received msg
+
+
+client = mqtt.Client("digi_mqtt_test")  # Create instance of client with client ID “digi_mqtt_test”
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker, port)
+client.loop_forever()  # Start networking daemon
