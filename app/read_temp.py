a20edf321404326080c13477a4c584186d2345d4
uschults <90207035+uschults@users.noreply.github.com>
2023-03-25 15:55:13 +0200

    Update read_temp.py

--- app/read_temp.py
+++ app/read_temp.py
@@ -1,16 +1,17 @@
 import glob
 
 datastring = ''
-for sensor in glob.glob("/sys/bus/w1/devices/28-00*/w1_slave"):
-    id = sensor.split("/")[5]
-    try:
-        f = open(sensor, "r")
-        data = f.read()
-        f.close()
-        if "YES" in data:
-            (discard, sep, reading) = data.partition(' t=')
-            t = float(reading) / 1000.0    #reports temperature in degrees C
-            datastring = datastring + id + ':' + str(t) + ','
+while True:
+    for sensor in glob.glob("/sys/bus/w1/devices/28-00*/w1_slave"):
+        id = sensor.split("/")[5]
+        try:
+            f = open(sensor, "r")
+            data = f.read()
+            f.close()
+            if "YES" in data:
+                (discard, sep, reading) = data.partition(' t=')
+                t = float(reading) / 1000.0    #reports temperature in degrees C
+                datastring = datastring + id + ':' + str(t) + ','
 
-    except:    
-        pass
+        except:    
+            pass
