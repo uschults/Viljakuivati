6562ece86e0e25b337808fff00ae8841a357a55c
uschults <90207035+uschults@users.noreply.github.com>
2023-06-06 22:25:41 +0300

    Update client.py

--- app/client.py
+++ app/client.py
@@ -142,7 +142,6 @@ def connect_mqtt():
             #publish("teade", motor_topics)
             for motor in motor_topics:
                 client.subscribe(motor)
-                publish("teade", motor)
             client.subscribe("fill_container_1")
             client.subscribe("fill_container_2")
             client.subscribe("check1")
@@ -186,28 +185,44 @@ def on_message(client, userdata, msg):
         #print("Connection check")
         publish("pistate", "Online")
     
-    elif(msg.topic == "fill_container_1" and data == "true"):
-        # check if another program is already running
-        if(not program_running.is_set()):
-            # check if level_buttons are connected
-            # pin 40 is the first container
-            if(level_buttons):
-                if(not (GPIO.input(8))):
-                    #print("Programm: täida punker")
-                    program_running.set()
-                    fill_thread = Thread(target= fill_container, args= (program_running, ))
-                    fill_thread.start()
+    elif(msg.topic == "fill_container_1"):
+        if(data == "true"):
+            # check if another program is already running
+            if(not program_running.is_set()):
+                # check if level_buttons are connected
+                # pin 40 is the first container
+                if(level_buttons):
+                    if(not (GPIO.input(8))):
+                        #print("Programm: täida punker")
+                        program_running.set()
+                        fill_thread = Thread(target= fill_container, args= (program_running, ))
+                        fill_thread.start()
+                    else:
+                        publish("teade","Punker juba täis")
+                        publish("fill_container_in", "false")
+                        #print("Punker 1 juba täis")
                 else:
+                    publish("teade","Ei ole tasemeandurit")
                     publish("fill_container_in", "false")
-                    publish("teade","Punker juba täis")
-                    #print("Punker 1 juba täis")
+                    #print("Ei ole tasemeandureid")
             else:
-                publish("teade","Ei ole tasemeandurit")
+                publish("teade","Teine program juba käib")
                 publish("fill_container_in", "false")
-                #print("Ei ole tasemeandureid")
-        
-    elif(msg.topic == "fill_container_1" and data == "false"):
-        program_running.clear()
+        else:
+            program_running.clear()
+
+    elif(msg.topic == "fill_container_2"):
+        if(data == "true"):
+            if(not program_running.is_set()):
+                publish("mootor1_in", "true")
+                publish("mootor2_in", "true")
+            else:
+                publish("teade","Teine program juba käib")
+                publish("fill_container_2_in", "false")
+        else:
+            publish("mootor1_in", "false")
+            publish("mootor2_in", "false")
+
 
 def mqtt_init():
     client = connect_mqtt()
