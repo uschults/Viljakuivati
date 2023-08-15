49332c99b61018435bef81031c3349283fd9975f
uschults <90207035+uschults@users.noreply.github.com>
2023-08-15 15:57:34 +0300

    Update save_data.py

--- app/save_data.py
+++ app/save_data.py
@@ -3,7 +3,7 @@ import csv
 
 
 def save_to_client (file_name, data_value):
-    with open("/home/pi/Desktop/" + file_name + ".csv", 'a+') as open_file:
+    with open("/home/pi/" + str(file_name) + ".csv", 'a+') as open_file:
         writer = csv.writer(open_file)
         writer.writerow(data_value)
      
