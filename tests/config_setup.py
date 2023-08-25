c81b8c08b9537b3e20cc7248a7e11bd8827d344a
uschults <90207035+uschults@users.noreply.github.com>
2023-08-26 02:00:44 +0300

    moving conffiles

--- tests/config_setup.py
+++ tests/config_setup.py
@@ -2,29 +2,12 @@ from configparser import ConfigParser
 config_object = ConfigParser()
 # all pins indicate physical pin number
 # pins for buttons
-config_object['BUTTON_PINS'] = {
-    "btn1" : "16",
-    "btn2" : "18"
-}
-
-# pins for temp sensor
-# uses onewire protocol
-config_object['TEMP_PINS'] = {
-    "temp_sens" : "7"
-}
 
 config_object['MOTOR_PINS'] = {
     'mootor1' : '29',
     'mootor2' : '31',
     'mootor3' : '33',
     'mootor4' : '35'
-}
-
-config_object['SETUP'] = {
-    'mqtt_server_ip' : '80.250.119.25',
-    'port' : '1883',
-    'username' : 'urmosc',
-    'password' : 'admin'
 }
 
 #Write the above sections to config.ini file
